992254d4d529fc943bb5389998a7e2f1b6d2ae0e
OgembaJr <emmanuel.ogemba@student.moringaschool.com>
2022-11-24 20:10:33 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(value){
+  if(value <= 400){
+    return 'This one is on me!';
+
+  }else if(value >2000 && value <=2500){
+    return 'I will gladly take your thirty bucks.';
+
+  }else {
+    return 'No can do.';
+  }
+     
   // Write your code here!
+}
+function ternaryCheckCity(city){
+  // Write your code here!
+   return city === "NYC" ? "Ok, sounds good." : 'No go.';
+
 }
 
-function ternaryCheckCity(){
+function switchOnCharmFromTip(key){
   // Write your code here!
-}
+  switch (key) {
+    case 'generous': 
+    return "Thank you so much.";
+      
+      break;
+    case 'not as generous': 
+        return "Thank you.";
+      break;
+    default: 
+         return "Bye.";
+  }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
 }
+ 
